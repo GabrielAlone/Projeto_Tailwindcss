69a878df0ae61a8cb2aa69de8aea757cc7f10349
Aluno <e023aluno@outlook.com>
2026-09-14 22:05:27 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Header from "./components/Header"
+
+
+
+import Footer from "./components/Footer"
+
+export default function App() { 
+  return ( 
+    <div className="min-h-screen bg-slate-100 text-slate-900">
+
+    <Header />
+
+    <main>
+      {/* <Hero />
+      <Recursos />
+      <Sobre />*/}
+    </main>
+
+     <Footer />
+
+    </div>
+  )
+}
